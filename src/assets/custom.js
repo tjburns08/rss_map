9658755140db2355202070719ca7f2016166a1dd
Tyler Burns <burns.tyler@gmail.com>
2023-05-04 18:10:45 +0200

    removed the custom.js script and the assets folder

--- src/assets/custom.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('#user-input').addEventListener('keydown', function(e) {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            document.querySelector('#value-enter').click();
-        }
-    });
-});
